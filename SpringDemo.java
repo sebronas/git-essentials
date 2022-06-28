74d6188ed393b1d8a0e17dc8792a36b4a33b2b37
Kristaps Sebris <sebriskristaps@gmail.com>
2022-06-28 10:33:37 +0300

    making changes in SpringDemo file

--- SpringDemo.java
+++ SpringDemo.java
@@ -8,6 +8,7 @@ public class SpringDemoJavaee14Application {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringDemoJavaee14Application.class, args);
+        System.out.println("Some changes");
     }
 
 }
